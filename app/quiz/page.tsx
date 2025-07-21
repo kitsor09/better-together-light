c5bbc163d9547a88c3ec1e415d1f18f5656b8df2
kitsor09 <skitso.ostik@gmail.com>
2025-07-21 10:37:51 +0200

    Create page.tsx

---
+++ app/quiz/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function QuizPage() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>💬 Couples Quiz</h1>
+      <p>This is where your quiz questions will appear.</p>
+      <p>You can take turns answering and reflecting together.</p>
+    </main>
+  );
+}
